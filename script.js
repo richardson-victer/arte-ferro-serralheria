a3d6cc96ce0795f70baa3bcd5d9f5a9979a5cc59
richardson-victer <130123112+richardson-victer@users.noreply.github.com>
2023-07-13 10:25:17 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const btnMobile = document.getElementById ('btn-mobile');
 
-function toggleMenu() {
-    const nav = document.getElementsById ('nav');
-    nav.classList.toggle('active')
-}
 
-btnMobile.addEventListener ('click', toggleMenu);
+
+
+
+
+
+// function mudouTamanho() {
+//     if (window.innerWidth >= 601) {
+//         menu.style.display = 'block'
+//     } else {
+//         menu.style.display ='none'
+//     }
+// }
+
+// function clickMenu(){
+//     if (menu.style.display == 'block') {
+//      menu.style.display = 'none'
+//     } else{
+//      menu.style.display = 'block'
+//     }
+//  }
